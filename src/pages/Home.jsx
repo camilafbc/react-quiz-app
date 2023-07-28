49593c8847e31228570a05cb6ac986c2b54cb3b7
Camila F. B. Coelho <camilafernanda914@gmail.com>
2023-07-27 22:50:58 -0300

    Camila
    Att de estilo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,13 @@ import { Link } from "react-router-dom"
 
 function Home(){
     return (
-        <div>
-            <h2>Teste seus conhecimentos</h2>
+        <div className="home_container">
+            <h1>
+                Quiz
+                <img src="https://img.icons8.com/fluency/96/000000/ask-question.png" alt="" />
+            </h1>
+            
+            <p>Teste seus conhecimentos sobre HTML, CSS e JavaScript!</p>
             <Link to="quiz">
                 <button>Começar</button>
             </Link>
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import questions from "../database.json";
-
 
 
 function Quiz(){
@@ -27,18 +27,19 @@ function Quiz(){
             setAnswers([...answers, false])
         }
     }
+
     
     return (
-        <div>
+        <>
             {!result ? (
                 <div className="quiz_container">
+                    <span>{questions[currentQuestion].id} de {questions.length}</span>
                     <h2>
-                        <span>{questions[currentQuestion].id}-</span>
                         {questions[currentQuestion].question}
                     </h2>
                     {questions[currentQuestion].options.map((option) => (
                         <button
-                            className="option" 
+                            className="option"
                             key={option}
                             onClick={() => {
                                 checkAnswer(option)
@@ -51,14 +52,20 @@ function Quiz(){
                         className="next"
                         onClick={handleQuestion}
                     >
-                        Próxima
+                        {currentQuestion === questions.length - 1 ? "Finalizar" : "Próxima >>"}
                     </button>
                 </div>
                 
             ) : (
-                <h2>Você acertou {answers.filter(ans => ans===true).length}</h2>
+                <div className="result_container">
+                    <img src="https://img.icons8.com/clouds/100/test-passed.png" alt="" />
+                    <h1>Você teve <br/> {answers.filter(ans => ans===true).length} acertos!</h1>
+                    <Link to="/">
+                        <button onClick={() => console.log(answers)}>Outra Vez!</button>
+                    </Link>
+                </div>
             )}
-        </div>
+        </>
     )
    
 }
